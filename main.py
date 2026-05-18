fef96a9422c5ab514797bea2b5145757742fa47f
Mike <michaelsal1904@gmail.com>
2026-05-18 17:53:11 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello, {name}!"}
+
+@app.get("/search")
+def search(q: str):
+    return {"you searched for": q}
