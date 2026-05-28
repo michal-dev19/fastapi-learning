b01318ac9def1a62ad6b9e27389eb315006f17c6
Michal <michaelsal1904@gmail.com>
2026-05-28 18:49:32 +0100

    added post, put, and patch

--- courier-app.py
+++ courier-app.py
@@ -1,11 +1,17 @@
 import sqlite3
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import uvicorn
 
 conn = sqlite3.connect("courier.db", check_same_thread=False)
 cursor = conn.cursor()
 
 app = FastAPI()
+
+
+class Driver(BaseModel):
+    name: str
+    email: str
 
 
 def init_db():
@@ -73,6 +79,43 @@ def delete_user(id: int):
     if cursor.rowcount == 0:
         raise HTTPException(status_code=404, detail="Item not found")
     return {"deletion": "Success"}
+
+
+# recieve incoming POST data into API
+@app.post("/drivers")
+def create_user(new_driver: Driver):
+    # try except block to catch errors
+    try:
+        # insert new data into database
+        cursor.execute(
+            "INSERT INTO users (name, email) VALUES (?, ?)",
+            (new_driver.name, new_driver.email),
+        )
+        conn.commit()
+    except sqlite3.Error:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    return {"new_driver": {"name": new_driver.name, "email": new_driver.email}}
+
+
+# update existing data within database
+@app.put("/drivers/{id}")
+def update_user(updated_driver: Driver, id: int):
+    # try except block for catching errors
+    try:
+        # updates specific data for given id
+        cursor.execute(
+            "UPDATE users SET name=?, email=? WHERE id=?",
+            (updated_driver.name, updated_driver.email, id),
+        )
+        # if no changes were made, raise error
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=404, detail="Item not found")
+        conn.commit()
+    except sqlite3.Error:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    return {
+        "updated_driver": {"name": updated_driver.name, "email": updated_driver.email}
+    }
 
 
 if __name__ == "__main__":
